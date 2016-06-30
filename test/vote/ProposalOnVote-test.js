31fb9a789cea43c44e91f8644bff48ccc4ded848
romanman <roman.mandeleil@gmail.com>
2016-06-30 09:20:26 +0000

    Typo

--- test/vote/ProposalOnVote-test.js
+++ test/vote/ProposalOnVote-test.js
@@ -28,13 +28,13 @@ describe('ProposalOnVote Contract Suite', function() {
   var proposalText = "Donald Trump For President of United States";
   
   /*inf*/
-	  if (compiled.errors){
-		console.log(compiled.errors)
-	  }
-	  else{
-		console.log("Comilation Success");  
-		console.log("");
-	  }
+      if (compiled.errors){
+        console.log(compiled.errors)
+      }
+      else{
+        console.log("Compilation Success");  
+        console.log("");
+      }
   /*inf*/
   
   var proposal;
@@ -52,134 +52,134 @@ describe('ProposalOnVote Contract Suite', function() {
   
   /*
     TestCase: test-deploy 
-	Description: deploying the contract, 
-	 validating that the deployment was good.
-	 The deployed contract will be used for 
-	 contract call testing in the following 
-	 test cases.
+    Description: deploying the contract, 
+     validating that the deployment was good.
+     The deployed contract will be used for 
+     contract call testing in the following 
+     test cases.
   */
   it('test-deploy', function(done) {
-	  /*inf*/console.log(" [test-deploy]");
-	  
-		sandbox.web3.eth.contract(JSON.parse(compiled.contracts['ProposalOnVote'].interface)).new(
-		proposalText, 
-		{
-			  
-			  /* contract creator */ 
-			  from: "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+      /*inf*/console.log(" [test-deploy]");
+      
+        sandbox.web3.eth.contract(JSON.parse(compiled.contracts['ProposalOnVote'].interface)).new(
+        proposalText, 
+        {
+              
+              /* contract creator */ 
+              from: "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
 
-			  /* contract bytecode */ 
-			  data: '0x' + compiled.contracts['ProposalOnVote'].bytecode			
-		}, 
-		  
-		function(err, contract) {
-				
-			if (err) {
-				done(err);
-			}
-			else if (contract.address){
-				proposal = contract;
-				done();
-			}			
-		});	  
-	  
+              /* contract bytecode */ 
+              data: '0x' + compiled.contracts['ProposalOnVote'].bytecode            
+        }, 
+          
+        function(err, contract) {
+                
+            if (err) {
+                done(err);
+            }
+            else if (contract.address){
+                proposal = contract;
+                done();
+            }            
+        });      
+      
   });
   
   
   /*
     TestCase: check-init 
-	Description: assert that the initiation value of 
-	             proposal text worked as expected
+    Description: assert that the initiation value of 
+                 proposal text worked as expected
   */
   it('check-init', function(done) {
-	/*inf*/console.log(" [check-init]");
+    /*inf*/console.log(" [check-init]");
 
-    /* Constant call no transaction required */	
-	var recivedText = proposal.getProposalText();
-	assert(recivedText, proposalText);
-		
-	done();
+    /* Constant call no transaction required */    
+    var recivedText = proposal.getProposalText();
+    assert(recivedText, proposalText);
+        
+    done();
   });
   
   /*
     TestCase: check-vote-yes 
-	Description: 
+    Description: 
   */
   it('check-vote-yes', function(done) {
-	/*inf*/console.log(" [check-vote-yes]");
-	
-	// sending transaction arbitrary signed
-	// no peer keystore is involved and that
-	// type of encoding can be used directly 
-	// out of any browser
-    funcABI = 	{ "constant": false, "inputs": [], "name": "voteYes", "outputs": [], "type": "function"};
+    /*inf*/console.log(" [check-vote-yes]");
+    
+    // sending transaction arbitrary signed
+    // no peer keystore is involved and that
+    // type of encoding can be used directly 
+    // out of any browser
+    funcABI =     { "constant": false, "inputs": [], "name": "voteYes", "outputs": [], "type": "function"};
     var func = new SolidityFunction(sandbox.web3,   funcABI, proposal.address);
-    var callData = func.toPayload([]).data;	
-	
-	sandbox.web3.eth.sendTransaction({
+    var callData = func.toPayload([]).data;    
+    
+    sandbox.web3.eth.sendTransaction({
         from: "0xdedb49385ad5b94a16f236a6890cf9e0b1e30392",
         to: proposal.address,
         gas: 200000,
         value: sandbox.web3.toWei(1, 'ether'),
-		data: callData
-	}, function(err, txHash) {
-		if (err) done(err);
-			
-		// we are waiting for blockchain to accept the transaction 
-		helper.waitForReceipt(sandbox.web3, txHash, assertVotedYes(done));	
-	});
-		 
+        data: callData
+    }, function(err, txHash) {
+        if (err) done(err);
+            
+        // we are waiting for blockchain to accept the transaction 
+        helper.waitForReceipt(sandbox.web3, txHash, assertVotedYes(done));    
+    });
+         
   });
   
   function assertVotedYes(done){
 
-    /* Constant call no transaction required */	
-	var votedYes = proposal.getVotedYes();
-	assert.equal(votedYes.toNumber(), 1);	
-	
-	done();
+    /* Constant call no transaction required */    
+    var votedYes = proposal.getVotedYes();
+    assert.equal(votedYes.toNumber(), 1);    
+    
+    done();
   }
   
   /*
     TestCase: check-vote-yes-in-loop 
-	Description: 
+    Description: 
   */
   it('check-vote-yes-in-loop', function(done) {
-	/*inf*/console.log(" [check-vote-yes]");
-	
-	// sending transaction arbitrary signed
-	// no peer keystore is involved and that
-	// type of encoding can be used directly 
-	// out of any browser
-    funcABI = 	{ "constant": false, "inputs": [], "name": "voteYes", "outputs": [], "type": "function"};
+    /*inf*/console.log(" [check-vote-yes]");
+    
+    // sending transaction arbitrary signed
+    // no peer keystore is involved and that
+    // type of encoding can be used directly 
+    // out of any browser
+    funcABI = { "constant": false, "inputs": [], "name": "voteYes", "outputs": [], "type": "function"};
     var func = new SolidityFunction(sandbox.web3,   funcABI, proposal.address);
-    var callData = func.toPayload([]).data;	
-	
-	async.times(10, function(n, next){
-		sandbox.web3.eth.sendTransaction({
-			from: "0xdedb49385ad5b94a16f236a6890cf9e0b1e30392",
-			to: proposal.address,
-			gas: 200000,
-			value: sandbox.web3.toWei(1, 'ether'),
-			data: callData
-		}, function(err, txHash) {
-			if (err) done(err);
-				
-			// we are waiting for blockchain to accept the transaction 
-			helper.waitForReceipt(sandbox.web3, txHash);	
-			next();
-		});	
-	}, function(err, users) {
+    var callData = func.toPayload([]).data;    
+    
+    async.times(10, function(n, next){
+        sandbox.web3.eth.sendTransaction({
+            from: "0xdedb49385ad5b94a16f236a6890cf9e0b1e30392",
+            to: proposal.address,
+            gas: 200000,
+            value: sandbox.web3.toWei(1, 'ether'),
+            data: callData
+        }, function(err, txHash) {
+            if (err) done(err);
+                
+            // we are waiting for blockchain to accept the transaction 
+            helper.waitForReceipt(sandbox.web3, txHash);    
+            next();
+        });    
+    }, function(err, users) {
 
-	  /* Constant call no transaction required */	
-	  var votedYes = proposal.getVotedYes();
-	
-	  log(votedYes);
-	  done();
-	});
-	
-	
-		 
+      /* Constant call no transaction required */    
+      var votedYes = proposal.getVotedYes();
+    
+      log(" *** voted yes: " +  votedYes.toNumber());
+      done();
+    });
+    
+    
+         
   });
   
   
@@ -190,9 +190,9 @@ describe('ProposalOnVote Contract Suite', function() {
   after(function(done) {
     
     /*inf*/console.log(" [sandbox stopping]");
-	sandbox.stop(done);
+    sandbox.stop(done);
 
-	/*inf*/ var end = new Date().getTime();
+    /*inf*/ var end = new Date().getTime();
     /*inf*/ time = (end - start)/1000; console.log(" < " + time  + "s >\n");
   });
 });
